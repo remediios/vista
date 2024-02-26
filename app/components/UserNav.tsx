2f5525579cfd7d1c5b9bba94653e8a72a09dc36d
Miguel Remédios <miguelremediios@gmail.com>
2024-02-26 20:32:21 +0000

    kinde authentication
    Added kinde authentication and respective environment variable for it to work
    Modified the usernav bar to incorporate such changes
    implemented kinde auth folder structure to allow the internal api of the service to work
    fixed the user logo picture when logged in

--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -7,25 +8,49 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { MenuIcon } from 'lucide-react';
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from '@kinde-oss/kinde-auth-nextjs/components';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 
-const UserNav = () => {
+const UserNav = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
         <div className="rounded-full border px-2 py-2 lg:px-4 lg:py-2 flex items-center gap-x-3">
           <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5" />
           <img
-            src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
+            src={
+              user?.picture ??
+              'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg'
+            }
             alt="Image of the user"
             className="rounded-full h-8 w-8 hidden lg:block"
           />
         </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-[200px]">
-        <>
-          <DropdownMenuItem>Register</DropdownMenuItem>
-          <DropdownMenuItem>Login</DropdownMenuItem>
-        </>
+        {user ? (
+          <>
+            <DropdownMenuItem>
+              <LogoutLink className="w-full">Logout</LogoutLink>
+            </DropdownMenuItem>
+          </>
+        ) : (
+          <>
+            <DropdownMenuItem>
+              <RegisterLink className="w-full">Register</RegisterLink>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <LoginLink className="w-full">Login</LoginLink>
+            </DropdownMenuItem>
+          </>
+        )}
       </DropdownMenuContent>
     </DropdownMenu>
   );
