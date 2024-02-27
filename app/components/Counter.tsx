5b528d966c90e71c0d1a0745b4576cc0255e4391
Miguel Remédios <miguelremediios@gmail.com>
2024-02-27 21:37:53 +0000

    Description page update and new functionality
    Description page inputs were implemented from title description price image upload and extra card
    This extra card contains counters for guests nr, bedroom nrs and bathrooms nrs
    Counter component implemenet successfully with respective logic restriction below zero

---
+++ app/components/Counter.tsx
@@ -0,0 +1,35 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import { Minus, Plus } from 'lucide-react';
+import { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="flex items-center gap-x-4">
+      <Button
+        variant="outline"
+        size="icon"
+        type="button"
+        onClick={() => {
+          if (count === 0) return;
+          setCount(count - 1);
+        }}
+      >
+        <Minus className="w-4 h-4 text-primary" />
+      </Button>
+      <p className="font-medium text-lg">{count}</p>
+      <Button
+        variant="outline"
+        size="icon"
+        type="button"
+        onClick={() => setCount(count + 1)}
+      >
+        <Plus className="w-4 h-4 text-primary" />
+      </Button>
+    </div>
+  );
+};
+
+export default Counter;
--- app/create/[id]/description/page.tsx
+++ app/create/[id]/description/page.tsx
@@ -1,8 +1,85 @@
+import Counter from '@/app/components/Counter';
+import { Card, CardHeader } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Textarea } from '@/components/ui/textarea';
+
 const DescriptionPage = () => {
   return (
-    <div>
-      <h1>Description page for category</h1>
-    </div>
+    <>
+      <div className="w-3/5 mx-auto">
+        <h2 className="text-3xl font-semibold tracking-tight">
+          Please describe your home in great detail!
+        </h2>
+      </div>
+      <form>
+        <div className="mx-auto w-3/5 mt-10 flex flex-col gap-y-5 mb-36">
+          <div className="flex flex-col gap-y-3">
+            <Label className="text-md">Title</Label>
+            <Input
+              type="text"
+              name="title"
+              required
+              placeholder="Short and simple..."
+            />
+          </div>
+          <div className="flex flex-col gap-y-3">
+            <Label className="text-md">Description</Label>
+            <Textarea
+              name="description"
+              required
+              placeholder="Please describe your home..."
+            />
+          </div>
+          <div className="flex flex-col gap-y-3">
+            <Label className="text-md">Price</Label>
+            <Input
+              type="number"
+              name="price"
+              required
+              placeholder="Price per night in EUR €"
+              min={10}
+            />
+          </div>
+          <div className="flex flex-col gap-y-3">
+            <Label className="text-md">Image</Label>
+            <Input name="image" type="file" required />
+          </div>
+
+          <Card>
+            <CardHeader className="flex flex-col gap-y-5">
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col">
+                  <h3 className="underline font-medium">Guests</h3>
+                  <p className="text-muted-foreground text-sm">
+                    How many guest do you want?
+                  </p>
+                </div>
+                <Counter />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col">
+                  <h3 className="underline font-medium">Bedrooms</h3>
+                  <p className="text-muted-foreground text-sm">
+                    How many rooms are you renting?
+                  </p>
+                </div>
+                <Counter />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col">
+                  <h3 className="underline font-medium">Bathrooms</h3>
+                  <p className="text-muted-foreground text-sm">
+                    How many bathrooms does your home have?
+                  </p>
+                </div>
+                <Counter />
+              </div>
+            </CardHeader>
+          </Card>
+        </div>
+      </form>
+    </>
   );
 };
 
