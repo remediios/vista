d371bb49074a10e6c18881622229aee6dc3116e3
Miguel Remédios <miguelremediios@gmail.com>
2024-02-28 00:56:19 +0000

    useCountries custom hook and address page component implementation

--- app/create/[id]/address/page.tsx
+++ app/create/[id]/address/page.tsx
@@ -1,5 +1,39 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
 const AddressPage = () => {
-  return <div>AddressPage</div>;
+  return (
+    <>
+      <div className="w-3/5 mx-auto">
+        <h2 className="text-3xl font-semibold tracking-tight transition-colors mb-10">
+          Where is your home located?
+        </h2>
+      </div>
+      <form>
+        <div className="w-3/5 mx-auto">
+          <div className="mb-5">
+            <Select required>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Select a country" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Countries</SelectLabel>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default AddressPage;
---
+++ app/lib/getCountries.ts
@@ -0,0 +1,20 @@
+import countries from 'world-countries';
+
+const countriesFormatted = countries.map((country) => ({
+  value: country.cca2,
+  label: country.name.common,
+  flag: country.flag,
+  latLang: country.latlng,
+  region: country.region,
+}));
+
+const useCountries = () => {
+  const getAllCountries = () => countriesFormatted;
+
+  const getCountryByValue = (countryValue: string) => {
+    return countriesFormatted.find(
+      (countryFind) => countryFind.value === countryValue
+    );
+  };
+  return { getAllCountries, getCountryByValue };
+};
