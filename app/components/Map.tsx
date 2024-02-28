03b5ddc6a3e563ee70f791b00cdab3490211cd54
Miguel Remédios <miguelremediios@gmail.com>
2024-02-28 01:54:28 +0000

    problem fixing macos

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,8 +1,14 @@
 'use client';
 
-import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useCountries } from '../lib/getCountries';
+import { icon } from 'leaflet';
+
+const ICON = icon({
+  iconUrl: '',
+  iconSize: [25, 41],
+});
 
 const Map = ({ locationValue }: { locationValue: string }) => {
   const { getCountryByValue } = useCountries();
@@ -13,12 +19,13 @@ const Map = ({ locationValue }: { locationValue: string }) => {
       scrollWheelZoom={false}
       className="h-[50vh] rounded-lg relative z-0"
       center={latLang ?? [52.505, -0.09]}
-      zoom={13}
+      zoom={8}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <Marker position={latLang ?? [52.505, -0.09]} icon={ICON} />
     </MapContainer>
   );
 };
